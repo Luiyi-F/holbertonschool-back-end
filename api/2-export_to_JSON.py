eb978ac1db75bfa01a0f8cdcf7e064bb74105153
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-13 11:12:30 -0500

    [TASK 2]: create a script for export json file

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -8,21 +8,22 @@ from requests import get
 def json_export():
     """export json"""
 
-    base_url = "https://jsonplaceholder.typicode.com/"
+    base_url = "https://jsonplaceholder.typicode.com"
     user_id = argv[1]
 
-    employee = get("{}/users/{}".format(base_url, user_id))
-    username = employee.json().get("username")
+    employee = get('{}/users/{}'.format(base_url, user_id))
+    username = employee.json().get('username')
 
-    tasks = get("{}/todos?userId={}".format(base_url, user_id))
-    task_dict = tasks.json()
-    json_dict = {user_id: [{"task": task.get("title"),
-                            "completed": task.get("completed"),
+    tasks = get('{}/todos?userId={}'.format(base_url, user_id))
+    task_list = tasks.json()
+
+    json_dict = {user_id: [{"task": task.get('title'),
+                            "completed": task.get('completed'),
                             "username": username}
-                           for task in task_dict]}
+                           for task in task_list]}
 
-    with open("{}.json".format(user_id), "w") as f:
-        json.dump(json_dict, f)
+    with open('{}.json'.format(user_id), 'w') as fp:
+        json.dump(json_dict, fp)
 
 
 if __name__ == "__main__":
